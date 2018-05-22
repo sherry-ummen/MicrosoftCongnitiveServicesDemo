c93e4727c63674f3da05da3f02f20367212707f0
Sherry Ummen <sherry.ummen@napa.fi>
2018-05-22 18:05:25 +0300

    Show image

--- ComputerVisionAzure/MainWindow.xaml.cs
+++ ComputerVisionAzure/MainWindow.xaml.cs
@@ -1,20 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
+﻿using System.Windows;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using ComputerVisionAzure.Service;
-using Microsoft.ProjectOxford.Face.Contract;
-using OpenCvSharp.Extensions;
 
 namespace ComputerVisionAzure {
     /// <summary>
@@ -24,7 +9,7 @@ namespace ComputerVisionAzure {
         public MainWindow() {
             InitializeComponent();
             var viewModel = new MainWindowViewModel();
-            viewModel.OnResult += (result) => LeftImage.Source = (BitmapSource)result;
+            viewModel.OnResult += (result) => Image.Source = (BitmapSource)result;
             DataContext = viewModel;
         }
 
